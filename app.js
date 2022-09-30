c9df1f615c7e2cc4bf23c353b409639853e2fa09
a1dake <49231334+a1dake@users.noreply.github.com>
2022-09-30 20:53:01 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+let message = document.querySelector('.message');
+let addButton = document.querySelector('.add');
+let todos = document.querySelector('.todos');
+
+let todo_list = [];
+
+if(localStorage.getItem('todo')) {
+    todo_list = JSON.parse(localStorage.getItem('todo'));
+    addMessage();
+}
+
+addButton.addEventListener('click', function(){
+    let new_todo = {
+        todo: message.value,
+        checked: false
+    };
+    if (message.value) {
+    todo_list.push(new_todo);
+    addMessage();
+    localStorage.setItem('todo', JSON.stringify(todo_list));
+    }
+});
+
+function addMessage() {
+    let addMessage = '';
+    todo_list.forEach(function(item, index){
+        addMessage += `
+        <li id='todo_${index}'>
+            <input type='checkbox' id='todo_${index}' ${item.checked ?  'checked': ''}>
+            <label for='todo_${index}'>${item.todo}</label>
+        </li>`;
+
+        todos.innerHTML = addMessage;
+    });
+    message.value = '';
+};
+
+todos.addEventListener('change', function(event){
+    let valueLabel = todos.querySelector('[for='+ event.target.getAttribute('id') + ']').innerHTML;
+    
+    todo_list.forEach(function(item){
+        if (item.todo === valueLabel){
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todo_list));
+        }
+    });
+});
